cf0bda6d35b6fa17440817614047492145ed6700
sindhu <amruta.talam@gmail.com>
2018-12-26 15:11:25 +0530

    Sindhu | included initialize() which is private method being called from constructor in Game2048 class.

--- src/main/java/controller/Game2048.java
+++ src/main/java/controller/Game2048.java
@@ -7,8 +7,14 @@ public class Game2048 {
 
     private Grid grid;
 
+
     Game2048(Grid grid) {
         this.grid = grid;
+        initialize();
+    }
+
+    private void initialize() {
+
     }
 
     public Grid play() {
